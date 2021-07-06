451e839a6e93ac35ef749db74bd725ae30fbb998
Sarp Ulas Kaya <sarpkaya2000@gmail.com>
2021-07-06 19:18:08 +0300

    Practiced with Razor Test Project
    -Tested two different approaches to frontend design with and without APIs.
    -Practiced more with implementing editing and deletion opertaion for the users.
    -Practiced more with using migrations to edit the database easily.

---
+++ Razor/PalRaiserRazor/PalRaiserRazor/Controllers/ProjectController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PalRaiserRazor.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PalRaiserRazor.Controllers
+{
+    [Route("api/Project")]
+    [ApiController]
+    public class ProjectController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Project.ToListAsync() });
+        }
+        
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var projectFromDB = await _db.Project.FirstOrDefaultAsync(u => u.ProjId == id);
+            if (projectFromDB == null)
+            {
+                return Json(new { success = false, message = "Error while deleting." });
+            }
+            _db.Project.Remove(projectFromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Deletion successful" });
+        }
+    }
+}
---
+++ Razor/PalRaiserRazor/PalRaiserRazor/Migrations/20210705185211_AddProjectToDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PalRaiserRazor.Migrations
+{
+    public partial class AddProjectToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    ProjId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjName = table.Column<string>(nullable: false),
+                    Summary = table.Column<string>(nullable: false),
+                    DetailedInfo = table.Column<string>(nullable: false),
+                    Type = table.Column<string>(nullable: false),
+                    AmountRaised = table.Column<int>(nullable: true),
+                    likeCount = table.Column<int>(nullable: true),
+                    DislikeCount = table.Column<int>(nullable: true),
+                    ReportCount = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.ProjId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
